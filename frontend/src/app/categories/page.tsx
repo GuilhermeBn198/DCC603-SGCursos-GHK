f8ec9ef2fc479984a4bd8335ea196e284e4979b4
Hugo Lima Romão <hugo8romao@gmail.com>
2023-06-01 13:10:54 -0400

    initializing category crud functions

--- frontend/src/app/categories/page.tsx
+++ frontend/src/app/categories/page.tsx
@@ -1,34 +1,110 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { Button } from '@nextui-org/react'
+import { Button, Input } from '@nextui-org/react'
 
 import { Content } from 'components/Content'
 
 import * as S from './styles'
+import { useSession } from 'next-auth/react'
+import { useForm } from 'react-hook-form'
+import { Trash } from '@styled-icons/octicons'
+
+export interface ResponseCategory {
+  data: Category[]
+  errors: any[]
+}
+
+export interface Category {
+  id: number
+  name: string
+}
+
+type Inputs = {
+  name: string
+}
 
 const Categories = () => {
+  const { data: session } = useSession()
+  const { register, handleSubmit, reset } = useForm<Inputs>()
+
+  const [categories, setCategories] = useState<Category[] | undefined>()
+
+  async function getCategories() {
+    if (session?.user.jwt) {
+      const response: ResponseCategory = await fetch(
+        `http://localhost:5050/api/courses/categories`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${session.user.jwt}`
+          }
+        }
+      ).then((r) => r.json())
+      setCategories(response.data)
+    }
+  }
+
+  async function createCategory({ name }: Inputs) {
+    if (session?.user.jwt) {
+      await fetch(`http://localhost:5050/api/courses/categories/new`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${session.user.jwt}`
+        },
+        body: JSON.stringify({
+          name
+        })
+      })
+    }
+    reset()
+    getCategories()
+  }
+
+  async function deleteCategory(categoryId: number) {
+    if (session?.user.jwt) {
+      await fetch(
+        `http://localhost:5050/api/courses/categories/delete/${categoryId}`,
+        {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${session.user.jwt}`
+          }
+        }
+      )
+    }
+    getCategories()
+  }
+
+  useEffect(() => {
+    getCategories()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [session?.user.jwt])
+
   return (
     <Content>
-      <h1>Categories</h1>
+      <h1>Categorias</h1>
 
-      <Button size="sm" style={{ width: 'fit-content' }}>
-        Criar categoria
-      </Button>
+      <S.CreateCategoryWrapper onSubmit={handleSubmit(createCategory)}>
+        <Input
+          aria-label="Nome da nova categoria"
+          placeholder="Nome da nova categoria"
+          {...register('name', { required: true })}
+        />
+        <Button type="submit" size="sm" style={{ width: 'fit-content' }}>
+          Criar categoria
+        </Button>
+      </S.CreateCategoryWrapper>
 
       <S.CategoryList>
-        <S.Category>Development</S.Category>
-        <S.Category>Business</S.Category>
-        <S.Category>Finance & Accounting</S.Category>
-        <S.Category>IT & Software</S.Category>
-        <S.Category>Office Productivity</S.Category>
-        <S.Category>Design</S.Category>
-        <S.Category>Marketing</S.Category>
-        <S.Category>Lifestyle</S.Category>
-        <S.Category>Photography & Video</S.Category>
-        <S.Category>Health & Fitness</S.Category>
-        <S.Category>Music</S.Category>
-        <S.Category>Teaching & Academics</S.Category>
+        {categories?.map((c) => (
+          <S.Category key={c.id}>
+            {c.name}
+            <Trash size={24} onClick={() => deleteCategory(c.id)} />
+          </S.Category>
+        ))}
       </S.CategoryList>
     </Content>
   )
--- frontend/src/app/categories/styles.ts
+++ frontend/src/app/categories/styles.ts
@@ -10,9 +10,22 @@ export const CategoryList = styled.div`
 export const Category = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 16px 0;
 
   & + & {
     border-top: 1px solid #EBEBF0;
   }
+
+  svg {
+    cursor: pointer;
+    fill: #f44336;
+  }
+`
+
+export const CreateCategoryWrapper = styled.form`
+  display: flex;
+  align-items: center;
+
+  gap: 0.8rem;
 `
