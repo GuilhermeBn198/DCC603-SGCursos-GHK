8c8130ff7d0d8f4d30e4a0b8a55d9c2c2c7b3860
Hugo Lima Romão <hugo8romao@gmail.com>
2023-06-15 16:51:08 -0400

    fix: adding more routes on auth middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,3 +1,12 @@
 export { default } from 'next-auth/middleware'
 
-export const config = { matcher: ['/dashboard/:path*'] }
+export const config = {
+  matcher: [
+    '/dashboard/:path*',
+    '/categories/:path*',
+    '/courses/:path*',
+    '/classes/:path*',
+    '/users/:path*',
+    '/certificates/:path*'
+  ]
+}
